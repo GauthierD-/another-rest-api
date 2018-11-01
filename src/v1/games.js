cb696f36f2f396199a5680726e670c167320c401
Gauthier Deroo <g.deroo@gmail.com>
2018-11-01 20:08:40 +0100

    create patch and delete games route

--- src/v1/games.js
+++ src/v1/games.js
@@ -1,17 +1,20 @@
 'use strict'
 
 const express = require('express')
-const { pick, isString, isNumber, isArray, isDate } = require('lodash')
+const isValid = require('date-fns/is_valid')
+const { ObjectID } = require('mongodb')
+const { pick, isString, isNumber, isArray } = require('lodash')
 
 const router = express.Router()
 
 const {
   getGames,
   getOneGame,
-  insertGame
-  // updateGame,
-  // deleteGame
+  insertGame,
+  updateGame,
+  deleteGame
 } = require('../controllers/games')
+const { getOnePublisher } = require('../controllers/publishers')
 
 router.get('/', (req, res, next) => {
   return getGames()
@@ -32,7 +35,6 @@ router.get('/:id', (req, res, next) => {
 router.post('/', (req, res, next) => {
   const { title, price, publisher, tags, releaseDate } = req.body
 
-  console.log('>>>', req.body)
   if (!title || !price || !publisher || !tags || !releaseDate) {
     return next(new Error('400 - missing informations'))
   }
@@ -42,7 +44,7 @@ router.post('/', (req, res, next) => {
     !isNumber(price) ||
     !isString(publisher) ||
     !isArray(tags) ||
-    !isDate(new Date(releaseDate))
+    !isValid(new Date(releaseDate))
   ) {
     return next(new Error('400 - Bad informations'))
   }
@@ -50,9 +52,79 @@ router.post('/', (req, res, next) => {
   let game = pick(req.body, ['title', 'price', 'publisher', 'tags', 'releaseDate'])
   game = Object.assign({}, game, { releaseDate: new Date(game.releaseDate) })
 
-  return insertGame(game)
+  // create game only if publisher exists
+  return getOnePublisher(game.publisher)
+    .then((result) => {
+      if (!result) {
+        throw new Error(`Publisher ${game.publisher} does not exist`)
+      }
+    })
+    .then(() => {
+      return insertGame(game)
+        .then((result) => {
+          return res.json({ data: result })
+        })
+    })
+    .catch(next)
+})
+
+router.patch('/:id', (req, res, next) => {
+  const { title, price, publisher, tags, releaseDate } = req.body
+
+  if (!isString(req.params.id) || !ObjectID.isValid(req.params.id)) {
+    throw new Error('400 - GameID is not correct')
+  }
+  if (title && !isString(title)) {
+    return next(new Error('400 - title is not a String'))
+  }
+  if (price && !isNumber(price)) {
+    return next(new Error('400 - price is not a Number'))
+  }
+  if (publisher && !ObjectID.isValid(publisher)) {
+    return next(new Error('400 - publisher is not correct'))
+  }
+  if (tags && !isArray(tags)) {
+    return next(new Error('400 - tags is not a Array'))
+  }
+  if (releaseDate && !isValid(new Date(releaseDate))) {
+    return next(new Error('400 - releaseDate is not a String date'))
+  }
+
+  let waitFor = Promise.resolve()
+  if (publisher) {
+    waitFor = getOnePublisher(publisher)
+      .then((result) => {
+        if (!result) {
+          throw new Error(`Publisher ${publisher} does not exist`)
+        }
+
+        return result
+      })
+  }
+
+  return waitFor
+    .then((result) => {
+      let updateData = pick(req.body, ['title', 'price', 'publisher', 'tags', 'releaseDate'])
+      if (updateData.releaseDate) {
+        updateData = Object.assign({}, updateData, { releaseDate: new Date(updateData.releaseDate) })
+      }
+
+      return updateGame({ id: req.params.id, updateData })
+    })
     .then((result) => {
       return res.json({ data: result })
+    })
+    .catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+  if (!isString(req.params.id) || !ObjectID.isValid(req.params.id)) {
+    throw new Error('400 - GameID is not correct')
+  }
+
+  return deleteGame({ id: req.params.id })
+    .then((result) => {
+      res.json({ data: result })
     })
     .catch(next)
 })
