461f743a1fe572a3c5aa4a9f6f9ca8e191f35d51
Gauthier Deroo <g.deroo@gmail.com>
2018-11-05 22:59:30 +0100

    fix Fix test with class stub (#6)

--- test/controllers/games.js
+++ test/controllers/games.js
@@ -4,30 +4,22 @@ const test = require('tape')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire').noCallThru()
 
-const TEST_DB = 'testdb'
-
 const toArrayStub = sinon.stub()
-const settingsStub = sinon.stub()
-
 const db = {
   collection: sinon.stub().returnsThis(),
   find: sinon.stub().returns({ toArray: toArrayStub })
 }
-settingsStub.withArgs('MONGO_MAIN_NAME').returns(TEST_DB)
 
-const gamesControllers = proxyquire('../../src/controllers/games', {
-  '../../config': {
-    get: settingsStub
-  },
-  '../lib/databases': {
-    databases: {
-      mongodb: {
-        [TEST_DB]: {
-          db: () => db
-        }
-      }
+class DatabaseStub {
+  constructor () {
+    this.client = {
+      db: () => db
     }
   }
+}
+
+const Games = proxyquire('../../src/controllers/games', {
+  '../lib/class_database': DatabaseStub
 })
 
 test('GetGames - should throw an Error if mongo fail', (assert) => {
@@ -35,8 +27,8 @@ test('GetGames - should throw an Error if mongo fail', (assert) => {
   toArrayStub.resetHistory()
   toArrayStub.rejects(new Error('TOARRAYERROR'))
 
-  gamesControllers
-    .getGames()
+  const myGames = new Games()
+  myGames.getGames()
     .then(() => {
       assert.end('SHOULD NOT RESOLVES')
     })
@@ -51,8 +43,8 @@ test('GetGames - should called find with toArray', (assert) => {
   toArrayStub.resetHistory()
   toArrayStub.resolves()
 
-  gamesControllers
-    .getGames()
+  const myGames = new Games()
+  myGames.getGames()
     .then(() => {
       assert.ok(toArrayStub.called)
     })
